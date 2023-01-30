32308009c7f8d2907bc103152693f8550cfe77a3
Alex Almeida <alex@oddadigitalsystem.no>
2023-01-30 15:44:23 +0100

    feat(WIP): Obter produto por Id

--- Comex.Web/Controllers/ProdutoController.cs
+++ Comex.Web/Controllers/ProdutoController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Comex.Web.Data.Dto;
+using Comex.Models;
+using Comex.Models.Extensoes;
 
 namespace Comex.Web.Controllers
 {
@@ -7,6 +9,8 @@ namespace Comex.Web.Controllers
     [Route("[controller]")]
     public class ProdutoController : ControllerBase
     {
+        private static List<Produto> _produtos = new List<Produto>();
+
         [HttpGet]
         public IActionResult RetornaMomento()
         {
@@ -17,9 +21,21 @@ namespace Comex.Web.Controllers
         public IActionResult AdicionarProduto([FromBody] CriarProdutoDto produto)
         {
             if (ModelState.IsValid)
-                return Ok(produto);
+            {
+                _produtos.Add(new Produto(produto.Nome, produto.PrecoUnitario, produto.QuantidadeEmEstoque, produto.Categoria));
+                return Ok(_produtos.Last());
+            }
             else
                 return BadRequest(produto);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterProdutoPorId(int id)
+        {
+            var produto = _produtos.FirstOrDefault(p => p.Id == id);
+            if (produto.IsNull())
+                return NotFound();
+            return Ok(produto);
         }
     }
 }
