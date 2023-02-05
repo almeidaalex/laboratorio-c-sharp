54392b8110395a9e010f9196e218d86ac4c1f472
Alex Almeida <alex@oddadigitalsystem.no>
2023-02-05 23:38:19 +0100

    feat: api para fazer CRUD de produto

--- Comex.Web/Controllers/ProdutoController.cs
+++ Comex.Web/Controllers/ProdutoController.cs
@@ -3,76 +3,73 @@ using Comex.Web.Data.Dto;
 using Comex.Models;
 using Comex.Models.Extensoes;
 
-namespace Comex.Web.Controllers
+namespace Comex.Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProdutoController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class ProdutoController : ControllerBase
+    private static List<Produto> _produtos = new List<Produto>();
+
+    // [HttpGet]
+    // public IActionResult RetornaMomento()
+    // {
+    //     return Ok(DateTime.Now);
+    // }
+
+    [HttpPost]
+    public IActionResult AdicionarProduto([FromBody] CriarProdutoDto produto)
     {
-        private static List<Produto> _produtos = new List<Produto>();
+        _produtos.Add(new Produto(produto.Nome, produto.PrecoUnitario, produto.QuantidadeEmEstoque, produto.Categoria));
+        var created = _produtos.Last();
+        return CreatedAtAction(nameof(ObterProdutoPorId), new { Id = created.Id }, created);
+    }
 
-        [HttpGet]
-        public IActionResult RetornaMomento()
-        {
-            return Ok(DateTime.Now);
-        }
+    [HttpGet("{id}")]
+    public IActionResult ObterProdutoPorId(int id)
+    {
+        var produto = _produtos.FirstOrDefault(p => p.Id == id);
+        if (produto.IsNull())
+            return NotFound();
+        return Ok(produto);
+    }
 
-        [HttpPost]
-        public IActionResult AdicionarProduto([FromBody] CriarProdutoDto produto)
-        {
-            if (ModelState.IsValid)
-            {
-                _produtos.Add(new Produto(produto.Nome, produto.PrecoUnitario, produto.QuantidadeEmEstoque, produto.Categoria));
-                var created = _produtos.Last();
-                return CreatedAtAction(nameof(ObterProdutoPorId), new {Id = created.Id }, created);
-            }
-            else
-                return BadRequest(produto);
-        }
+    [HttpGet]
+    public IActionResult ObterProdutos(string? pesquisa, int? preco)
+    {
+        if (string.IsNullOrWhiteSpace(pesquisa))
+            return Ok(_produtos);
+        var produtos = _produtos.Where(p => p.Categoria == pesquisa);
+        if (produtos.IsNull())
+            return NotFound();
+        return Ok(produtos);
+    }
 
-        [HttpGet("{id}")]
-        public IActionResult ObterProdutoPorId(int id)
-        {
-            var produto = _produtos.FirstOrDefault(p => p.Id == id);
-            if (produto.IsNull())
-                return NotFound();
-            return Ok(produto);
-        }
 
-        [HttpPut("{id}")]
-        public IActionResult AtualizarProduto([FromBody] CriarProdutoDto atualizarProduto, int id)
-        {
-            var produto = _produtos.FirstOrDefault(p => p.Id == id);
-            if (produto.IsNull())
-                return NotFound();
+    [HttpPut("{id}")]
+    public IActionResult AtualizarProduto([FromBody] AtualizarProdutoDto atualizarProduto, int id)
+    {
+        var produto = _produtos.FirstOrDefault(p => p.Id == id);
+        if (produto.IsNull())
+            return NotFound();
 
-            if (ModelState.IsValid)
-            {
-                produto.Nome = atualizarProduto.Nome;
-                produto.PrecoUnitario = atualizarProduto.PrecoUnitario;
-                produto.QuantidadeEmEstoque = atualizarProduto.QuantidadeEmEstoque;
-                produto.Categoria = atualizarProduto.Categoria;
-            }
-            else
-            {
-                return BadRequest(atualizarProduto);
-            }
+        produto.Nome = atualizarProduto.Nome;
+        produto.PrecoUnitario = atualizarProduto.PrecoUnitario;
+        produto.QuantidadeEmEstoque = atualizarProduto.QuantidadeEmEstoque;
+        produto.Categoria = atualizarProduto.Categoria;
 
-            return Ok(produto);
-        }
+        return Ok(produto);
+    }
 
-        [HttpDelete("{id}")]
-        public IActionResult RemoverProduto(int id)
-        {
-            var produto = _produtos.FirstOrDefault(p => p.Id == id);
-            if (produto.IsNull())
-                return NotFound();
+    [HttpDelete("{id}")]
+    public IActionResult RemoverProduto(int id)
+    {
+        var produto = _produtos.FirstOrDefault(p => p.Id == id);
+        if (produto.IsNull())
+            return NotFound();
 
-            _produtos.Remove(produto);
+        _produtos.Remove(produto);
 
-            return NoContent();
-        }
+        return NoContent();
     }
 }
-
-
