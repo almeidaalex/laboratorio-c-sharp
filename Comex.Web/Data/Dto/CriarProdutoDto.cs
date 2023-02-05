1b4502a3ba4c2b2d16462d4065b57df427d39232
Alex Almeida <alex@oddadigitalsystem.no>
2023-02-05 23:37:22 +0100

    core: mudando para file scoped namespace

--- Comex.Web/Data/Dto/CriarProdutoDto.cs
+++ Comex.Web/Data/Dto/CriarProdutoDto.cs
@@ -1,21 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
-namespace Comex.Web.Data.Dto
+namespace Comex.Web.Data.Dto;
+
+public class CriarProdutoDto
 {
-    public class CriarProdutoDto
-    {
-        [Required]
-        [StringLength(50, MinimumLength = 5)]
-        public string Nome { get; set; }
+    [Required]
+    [StringLength(50, MinimumLength = 5)]
+    public string Nome { get; set; }
 
-        [Required]
-        public string Categoria { get; set; }
+    [Required]
+    public string Categoria { get; set; }
 
-        [Range(0.01, 100_000_000)]
-        public decimal PrecoUnitario { get; set; }
+    [Range(0.01, 100_000_000)]
+    public decimal PrecoUnitario { get; set; }
 
-        [Range(1, int.MaxValue)]
-        public int QuantidadeEmEstoque { get; set; }
-    }
+    [Range(1, int.MaxValue)]
+    public int QuantidadeEmEstoque { get; set; }
 }
