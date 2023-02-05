28a9a2cb978c6870499051afb66a6e069f1473a5
Alex Almeida <alex@oddadigitalsystem.no>
2023-02-05 23:37:11 +0100

    core: adiciona um dto para atualizar produto

---
+++ Comex.Web/Data/Dto/AtualizarProdutoDto.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Comex.Web.Data.Dto;
+
+public class AtualizarProdutoDto
+{
+    [Required]
+    [StringLength(50, MinimumLength = 5)]
+    public string Nome { get; set; }
+
+    [Required]
+    public string Categoria { get; set; }
+
+    [Range(0.01, 100_000_000)]
+    public decimal PrecoUnitario { get; set; }
+
+    [Range(1, int.MaxValue)]
+    public int QuantidadeEmEstoque { get; set; }
+}
